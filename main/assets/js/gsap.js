39302b4eb76bfad5d0656c9510591295ea302366
jasongithui <jasongithui@gmail.com>
2025-09-13 18:11:48 +0300

    bg color project-image

--- main/assets/js/gsap.js
+++ main/assets/js/gsap.js
@@ -46,36 +46,50 @@
             markers: false, // Set to true for debugging
           },
         });
-        // //  Selected projects section
-        // gsap.to(".gsap-selected", {
-        //   x: "0%",
-        //   rotation: 360,
-        //   ease: "power1.out",
-        //   duration: 0.6,
-        //   stagger: { amount: 0.2 },
-        //   scrollTrigger: {
-        //     trigger: ".section-home-projects",
-        //     start: "top 65%", // When section top hits bottom of viewport
-        //     end: "top ", // When section center hits center of viewport
-        //     scrub: true, // Link animation to scroll progress
-        //     markers: false, // Set to true for debugging
-        //   },
-        // });
+        gsap.to(".smiley", {
+          rotation: 810,
+          duration: 1,
+          ease: "power2.out",
+          
+          stagger: { amount: 0.2 },
+          scrollTrigger: {
+            trigger: ".section-home-intro",
+            start: "top 100%", 
+            end: "top ", 
+            scrub: true, // Link animation to scroll progress
+            markers: false, // Set to true for debugging
+          },
+        });
+        //  Selected projects section
+        gsap.to(".gsap-selected", {
+          
+          rotation: 360,
+          ease: "power1.out",
+          duration: 0.6,
+          stagger: { amount: 0.2 },
+          scrollTrigger: {
+            trigger: ".section-home-projects",
+            start: "top 60%", // When section top hits bottom of viewport
+            end: "top ", // When section center hits center of viewport
+            scrub: true, // Link animation to scroll progress
+            markers: false, // Set to true for debugging
+          },
+        });
 
-        // gsap.to(".gsap-projects", {
-        //   x: "0%",
-        //   rotation: -360,
-        //   ease: "power1.out",
-        //   duration: 0.6,
-        //   stagger: { amount: 0.2 },
-        //   scrollTrigger: {
-        //     trigger: ".section-home-projects",
-        //     start: "top 60%", // When section top hits bottom of viewport
-        //     end: "top ", // When section center hits center of viewport
-        //     scrub: true, // Link animation to scroll progress
-        //     markers: false, // Set to true for debugging
-        //   },
-        // });
+        gsap.to(".gsap-projects", {
+          
+          rotation: -360,
+          ease: "power1.out",
+          duration: 0.6,
+          stagger: { amount: 0.2 },
+          scrollTrigger: {
+            trigger: ".section-home-projects",
+            start: "top 60%", // When section top hits bottom of viewport
+            end: "top ", // When section center hits center of viewport
+            scrub: true, // Link animation to scroll progress
+            markers: false, // Set to true for debugging
+          },
+        });
 });
 const tl = gsap.to;
 /* Progress Event */
